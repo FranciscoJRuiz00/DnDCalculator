7c0cb46674b6388092f4af17da205f4a3106cbcc
FranciscoJRuiz00 <95589203+FranciscoJRuiz00@users.noreply.github.com>
2021-12-05 14:13:08 -0600

    Add files via upload

---
+++ D&DCalculator/src/modelo/usuarios.java
@@ -0,0 +1,47 @@
+
+package modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class usuarios {
+    
+    private int id;
+    private int id_tipo;
+    private String usuario;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_tipo() {
+        return id_tipo;
+    }
+
+    public void setId_tipo(int id_tipo) {
+        this.id_tipo = id_tipo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
